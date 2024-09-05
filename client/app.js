6925649485bbfc268a6324b5d7ea51da0e5ec8df
TumeloMkwambe <166211506+TumeloMkwambe@users.noreply.github.com>
2024-09-05 12:19:47 +0000

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({message: "client"});
-);
+app.get("/", (req, res) => res.send("What the fuck"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
