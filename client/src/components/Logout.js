a91f2a1b14e496154424f0ac0c511d8bc77c197f
TumeloMkwambe <2446873@students.wits.ac.za>
2024-09-12 13:51:01 +0000

    restructure

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from 'react-router-dom';
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
