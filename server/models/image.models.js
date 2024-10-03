2beb5fe73e57aa92e679bef71037fb7e585ec67d
Clement Jele <clemyjele@gmail.com>
2024-10-03 18:00:55 +0200

    Delete server directory

--- server/models/image.models.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const ImageSchema = new mongoose.Schema({
-    imageUrl: { type: String, required: true }
-  });
-  const Images = mongoose.model('Image', ImageSchema);
-
-  module.exports = Images;
