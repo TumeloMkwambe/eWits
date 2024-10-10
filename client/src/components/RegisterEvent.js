5b1c5edca5b66972bea9e84cf2a47c9c13413764
TebogoSeopa20 <seopatebogo13@gmail.com>
2024-10-10 23:09:15 +0200

    The tickets

--- client/src/components/RegisterEvent.js
+++ client/src/components/RegisterEvent.js
@@ -1,3 +1,5 @@
+
+
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
@@ -192,5 +194,3 @@ const RegisterEvent = () => {
 };
 
 export default RegisterEvent;
-
-
