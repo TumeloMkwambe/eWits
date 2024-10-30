0fd64d88bb6edf7a719603fbfb8a02720863c4fd
TebogoSeopa20 <seopatebogo13@gmail.com>
2024-10-30 12:05:51 +0200

    searchbar

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,54 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../../components/sidebar.js';
 import FavouriteEvents from '../../components/favouriteEvents.js';
 import UpcomingEvents from '../../components/upcomingEvents.js';
 import FilteredEvents from '../../components/filteredEvents.js';
 import EventList from '../../components/search.js';
+import SearchBar from '../../components/searchBar.js';
 import '../../globals.css';
 
-
 const Home = () => {
+    const [searchQuery, setSearchQuery] = useState('');
 
     const Types = [
-        'Sports',
-        'Religion',
-        'Education',
-        'Music',
-        'Arts and Culture',
-        'Business and Networking',
-        'Food and Drink',
-        'Community and Social',
-        'Health and Wellness',
-        'Charity and Fundraising',
-        'Technology',
-        'Family',
-      ];
+        'Sports', 'Religion', 'Education', 'Music', 'Arts and Culture', 'Business and Networking', 
+        'Food and Drink', 'Community and Social', 'Health and Wellness', 'Charity and Fundraising', 
+        'Technology', 'Family'
+    ];
 
     return (
         <div className="DashboardContainer">
             <Sidebar/>
             <div className="ContentArea">
-                <div className="home">       
+                <div className="home"> 
+                    <SearchBar setSearchQuery={setSearchQuery} />    
                     <div className="main-content">
-                        <div>
-                            <h2 className='title-home'>
-                                Favourite Events
-                            </h2>
-                        </div>
-                        <div className="past-events-container">
-                            
-                            <FavouriteEvents />
-                        </div>
 
-                        {Types.map( type => {
-                            return <div>
+                        {Types.map(type => (
+                            <div key={type}>
                                 <h2 className='upcoming-events'>{type}</h2>
                                 <div className="past-events-container">
-                                    < FilteredEvents type={type} />
+                                    <FilteredEvents type={type} searchQuery={searchQuery} />
                                 </div>
                             </div>
-                        })}
-
+                        ))}
                     </div>  
                 </div>
             </div>
