708f2cc31934a66f07e01cae17e416c9ce70392e
TebogoSeopa20 <seopatebogo13@gmail.com>
2024-11-01 15:15:16 +0200

    still

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -48,3 +48,56 @@ const Login =  () => {
 };
 
 export default Login;
+
+
+
+// import React, { useEffect } from 'react';
+// import { useAuth0 } from "@auth0/auth0-react";
+// import { useNavigate } from 'react-router-dom';
+// import '../globalStyle.css';
+// import axios from 'axios';
+
+// const postUser = async (name, email) => {
+//   const user = {
+//     name: name,
+//     email: email,
+//     liked_events: [],
+//     my_events: []
+//   }
+
+//   try {
+//     await axios.post(`${process.env.REACT_APP_USER_URI}/api/users/create`, user, {
+//       headers: {
+//         'Content-Type': 'application/json',
+//       }
+//     }).then( response => {
+//       sessionStorage.setItem('user', JSON.stringify(response.data));
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// const Login =  () => {
+//   const { user, loginWithRedirect, isAuthenticated } = useAuth0();
+//   const navigate = useNavigate();
+//   useEffect(() => {
+//     const handleLogin = async () => {
+//       if (isAuthenticated) {
+//         // Wait for postUser to finish before navigating
+//         await postUser(user.name, user.email);
+//         navigate('/home');
+//       }
+//     };
+//     handleLogin();
+//   }, [isAuthenticated, user, navigate]);
+//   return (
+//     <div>
+//       {/* <h2>Welcome! Please log in.</h2> */}
+//       <button onClick={() => loginWithRedirect()} className='Login'>Log In</button>
+//     </div>
+//   );
+  
+// };
+
+// export default Login;
